42663deefd7a15cae840cade67ffc1c1fc7a21e3
radin <dlwlsgml8847@gmail.com>
2025-03-19 03:16:00 +0900

    [Feat/#113] GA4 패키지 추가

--- ILSANG/Sources/Views/ILSANGApp.swift
+++ ILSANG/Sources/Views/ILSANGApp.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAnalytics
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct ILSANGApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     @AppStorage("isLogin") var isLogin = Bool()
     
     @State private var isTutorialVisible = Bool()
