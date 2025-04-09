8b69f57cf29f706df89c4fde396a28d3e9e1518a
radin <dlwlsgml8847@gmail.com>
2025-04-09 15:52:52 +0900

    [Feat/#316] 퀘스트 즐겨찾기 기능 추가 (FavoriteService - UI는 즉시 토글, 서버 반영은 디바운싱 처리)

--- ILSANG/Sources/Global/View/FilterPicker.swift
+++ ILSANG/Sources/Global/View/FilterPicker.swift
@@ -96,6 +96,7 @@ enum QuestFilterType: String, Hashable, CustomStringConvertible, CaseIterable {
     case pointHighest = "포인트 높은 순"
     case pointLowest = "포인트 낮은 순"
     case popular = "인기순"
+    case favorite = "즐겨찾기만"
     
     var description: String {
         return self.rawValue
@@ -107,7 +108,8 @@ enum EventQuestFilterType: String, Hashable, CustomStringConvertible, CaseIterab
     case pointHighest = "포인트 높은 순"
     case pointLowest = "포인트 낮은 순"
     case popular = "인기순"
-    
+    case favorite = "즐겨찾기만"
+
     var description: String {
         return self.rawValue
     }
--- ILSANG/Sources/Views/Home/HomeView.swift
+++ ILSANG/Sources/Views/Home/HomeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 // TODO: 에러처리 재정의 필요
 struct HomeView: View {
-    @State var vm: HomeViewModel = HomeViewModel(questNetwork: QuestNetwork(), rankNetwork: RankNetwork(), bannerNetwork: BannerNetwork())
+    @State var vm: HomeViewModel = HomeViewModel(questNetwork: QuestNetwork(), rankNetwork: RankNetwork(), bannerNetwork: BannerNetwork(), favoriteService: FavoriteService(favoriteNetwork: FavoriteNetwork()))
     @EnvironmentObject var sharedState: SharedState
     @Environment(\.redactionReasons) var redactionReasons
     
@@ -48,8 +48,15 @@ struct HomeView: View {
         .background(Color.background)
         .sheet(isPresented: $vm.showQuestSheet) {
             let tall = vm.selectedQuest.isRepeatQuest || vm.selectedQuest.missionType == .image
-            
-            QuestDetailView(vm: QuestDetailViewModel(quest: vm.selectedQuest, questNetwork: QuestNetwork())) {
+            QuestDetailView(
+                vm: QuestDetailViewModel(
+                    quest: vm.selectedQuest,
+                    questNetwork: QuestNetwork(),
+                    onUpdate: { quest in
+                        vm.toggleFavoriteStatus(quest: quest)
+                    }
+                )
+            ) {
                 vm.onQuestApprovalTapped()
             }
             .presentationCornerRadius(24)
@@ -224,10 +231,11 @@ struct HomeView: View {
                         ForEach(vm.recommendQuestList, id: \.id) { quest in
                             QuestItemView(
                                 quest: quest,
-                                style: RecommendStyle()) {
-                                    AnalyticsService.logEvent(.homeRecommendQuestClick(questId: quest.id))
-                                    vm.onQuestTapped(quest: quest)
-                                }
+                                style: RecommendStyle()
+                            ) {
+                                AnalyticsService.logEvent(.homeRecommendQuestClick(questId: quest.id))
+                                vm.onQuestTapped(quest: quest)
+                            }
                         }
                     }
                     .padding(.horizontal, LayoutConstants.horizontalPadding)
